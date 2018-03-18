55a71c6f2a551aaf66c6e4cbe10590e979a505dd
Dmitriy Merkurev <dmerkurev@avito.ru>
2018-03-18 04:14:26 +0300

    initial verifying bash script + test contracts

---
+++ verify.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+VERIFIER_IMAGE=dimorinny/ethereum-contract-verifier:1.0.0
+
+CONTRACTS_DIRECTORY=contracts
+
+function getContracts () {
+    ls -d ${CONTRACTS_DIRECTORY}/*
+}
+
+function verify () {
+    CONTRACTS=$(getContracts)
+    FAILED_CONTRACTS=()
+
+    for contract in ${CONTRACTS}
+    do
+        echo "Starting verifying directory: ${contract}..."
+
+        docker run \
+            -v $(pwd)/${contract}:/contract \
+            ${VERIFIER_IMAGE} \
+            ethereum-contract-verifier verify /contract
+
+        if [ $? -ne 0 ]; then
+            FAILED_CONTRACTS+=" ${contract}"
+        fi
+    done
+
+    if [ -n "$FAILED_CONTRACTS" ]; then
+        echo "Validation failed for${FAILED_CONTRACTS}"
+    fi
+}
+
+verify
