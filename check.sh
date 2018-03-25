43e5f7e0befe7f3b1d7007b9c29d6f4af4741ae8
Dmitriy Merkurev <dmerkurev@avito.ru>
2018-03-26 01:47:09 +0300

    check security options

--- check.sh
+++ check.sh
@@ -5,22 +5,27 @@ REPOSITORY=dimorinny/ethereum-verified-contracts
 setup() {
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
+
+  cat ./.id_rsa > /.ssh/id_rsa
+
+  cat /.ssh/id_rsa
+  cat ./.id_rsa
 }
 
-commit() {
-  git checkout -b master
-  git add contracts/**/*
-  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
-}
-
-push() {
-  git remote add origin-pages https://${GH_TOKEN}@github.com/${REPOSITORY}.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin-pages master
-}
+#commit() {
+#  git checkout -b master
+#  git add contracts/**/*
+#  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+#}
+#
+#push() {
+#  git remote add origin-pages https://${GH_TOKEN}@github.com/${REPOSITORY}.git > /dev/null 2>&1
+#  git push --quiet --set-upstream origin-pages master
+#}
 
 ./check.js
 if [ $? -eq 0 ]; then
   setup
-  commit
-  push
+#  commit
+#  push
 fi
